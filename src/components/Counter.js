129fe2197081d406c4dd11a626fff8abfa0d562f
Mark Atherton <mark@insightinternet.co.uk>
2021-12-06 16:24:57 +0000

    added this.props increment and decrement to appropriate handlers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -30,11 +30,14 @@ const Counter = () => {
 };
 
 class Counter extends Component {
+  incrementHandler() {
+    this.props.increment;
+  }
+  decrementHandler() {
+    this.props.decrement;
+  }
+  toggleCounterHandler() {}
   render() {
-    incrementHandler() {}
-    decrementHandler() {}
-    toggleCounterHandler() {}
-    
     return (
       <main className={classes.counter}>
         <h1>Redux Counter</h1>
@@ -51,15 +54,15 @@ class Counter extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    counter: state.counter
-  }
-}
+    counter: state.counter,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    increment: () => dispatch({ type: 'INCREMENT' }),
+    increment: () => dispatch({ type: "INCREMENT" }),
     decrement: () => dispatch({ type: "DECREMENT" }),
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
