2db231ad6c4b23ebfa83c47a900e39f09fb297b8
Mark Atherton <mark@insightinternet.co.uk>
2021-12-06 15:57:00 +0000

    added JSX to class based component below functional one

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
@@ -27,5 +28,21 @@ const Counter = () => {
     </main>
   );
 };
+
+class Counter extends Component {
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{counter}</div>
+        <div>
+          <button onClick={incrementHandler}>Increment</button>
+          <button onClick={decrementHandler}>Decrement</button>
+        </div>
+        <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
 
 export default Counter;
