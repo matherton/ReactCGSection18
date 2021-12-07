a2092db430970d2e89b091c794760ad44fa4af6a
Mark Atherton <mark@insightinternet.co.uk>
2021-12-07 10:27:31 +0000

    end of lesson 237: added increase action in store that uses action.amount to define how much the button increases the increment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,6 +9,10 @@ const Counter = () => {
     dispatch({ type: "INCREMENT" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: "DECREMENT" });
   };
@@ -21,6 +25,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
