65e6ef24e5a3e2f6a4a311b2aa472ce1d9140312
Mark Atherton <mark@insightinternet.co.uk>
2021-12-08 11:52:23 +0000

    end of lesson 241: added body of createSlice that looks like you can mutate state directly but it adds all other states automatically

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.counter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     return {
