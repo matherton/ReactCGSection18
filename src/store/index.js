331b7446d70291013c3712a80fd1fc99cab16be3
Mark Atherton <mark@insightinternet.co.uk>
2021-12-08 12:38:01 +0000

    replaced amount with payload as thats the default name redux-toolkit expects

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
     toggleCounter(state) {
       state.counter = !state.showCounter;
