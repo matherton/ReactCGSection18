70b40e8589fc31c4b5cb6bc3f0bba3c9da0755ee
Mark Atherton <mark@insightinternet.co.uk>
2021-12-08 14:01:12 +0000

    altered configureStore to have 2 reducer slices

--- src/store/index.js
+++ src/store/index.js
@@ -39,7 +39,10 @@ const authSlice = createSlice({
 });
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 export const counterActions = counterSlice.actions;
